2b7d7bfa228d2520f59dcd9f003b3b705ead2cfe
Luis Hernandez <luishrd@gmail.com>
2019-03-18 08:03:12 -0600

    adds solution code and instructions

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -2,6 +2,20 @@ module.exports = {
   add,
 };
 
-function add() {
-  return null;
+// function add(a = 0, b = 0) {
+//   return a + b;
+// }
+
+// function add(args) {
+//   return Array.from(arguments).reduce((sum, value) => {
+//     return sum + value;
+//   }, 0);
+// }
+
+function add(args) {
+  const values = Array.isArray(args) ? args : Array.from(arguments);
+
+  return values.reduce((sum, value) => {
+    return sum + value;
+  }, 0);
 }
