7e693d48c3635fce9e7b938e12f2f096fbc5a004
Ladrillo <gabriel@cabrejas.net>
2021-11-10 18:43:03 +0100

    wip

--- car/car.spec.js
+++ car/car.spec.js
@@ -4,17 +4,40 @@ const Car = require('./car') // Bring this _after_ failing `expect(Car).toBeDefi
 // END OF CLASS NOTE: we can mock libraries
 
 // 'describe' blocks organize tests and can be nested
-describe('Intro to unit testing', () => {
-  // This test is useful to confirm the tooling is working
-  // Explain jest globals, it and test (show the docs for globals)
-  it('works', () => { // THIS is the test
-    expect(2 + 2).toBe(4) // Assertion (comparing actual vs. expected)
+describe('Intro to testing', () => {
+  test('sanity', () => { // test
+    // assertions are usually closely related
+    expect(true).toBe(true) // assertion
+    expect(true).not.toBe(false) // another assertion
   })
-  test('toBe vs. toEqual', () => { // THIS is another test (empty tests are false positives)
-    expect({}).not.toBe({}) // Use `toEqual` when comparing composite shapes
-    expect({}).toEqual({}) // One test can have multiple **related** assertions
+
+  it('works', () => {
+    const expectedValue = 8
+    const actualValue = 8 // this usually comes from the function we're testing
+    expect(actualValue).toBe(expectedValue)
   })
 })
+
+describe('comparing values', () => {
+  test('objects and arrays', () => {
+    expect({}).not.toBe({}) // toBe does not work with objects like we expect!
+    expect([]).not.toBe([])
+    // if one assertion fails, the whole test fails!
+    expect({}).toEqual({})
+    // these are better than toEqual:
+    expect({ a: 1, b: 2 }).toMatchObject({ b: 2 })
+    expect({ a: 1, b: 2 }).toHaveProperty('a')
+    expect({ a: 1, b: 2 }).toHaveProperty('b', 2)
+    // the actual usually comes from a function we're testing
+    function foo() { return {} } // comes from a different file
+    expect(foo()).toEqual({}) // the actual comes from invoking the function
+    // if objects are truly the same then toBe works just fine
+    const a = 1
+    const b = a
+    expect(b).toBe(a)
+  })
+})
+
 // Unit tests test the smallest unit of a program: a class or a function
 describe('Car class', () => {
   let prius // Let's leverage closures in the following blocks
